3f92df392b06a3efee6d4390d4a9c4f12c7d190d
Caio Sales <caio.cesar@liven.tech>
2022-10-11 16:01:47 -0300

    feat: ensure singup controller returns 400 if no name is provided

---
+++ src/presentation/controllers/singup.spec.ts
@@ -0,0 +1,17 @@
+import { SingUpController } from "./singup";
+
+describe("SignUp Controller", () => {
+  test("should return 400 if no nome is provided", () => {
+    const sut = new SingUpController();
+    const httpRequest = {
+      body: {
+        name: undefined,
+        email: "any@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
---
+++ src/presentation/controllers/singup.ts
@@ -0,0 +1,5 @@
+export class SingUpController {
+  handle(httpResquest: any): any {
+    return { statusCode: 400 };
+  }
+}
