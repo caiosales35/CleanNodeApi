4bab1496516f60658b2cb518d5e889e2d33c1463
Caio Sales <caio.cesar@liven.tech>
2023-02-12 21:33:10 -0300

    feat: ci config

--- src/presentation/controllers/singup/singup-controller-protocols.ts
+++ src/presentation/controllers/singup/singup-controller-protocols.ts
@@ -1,6 +1,6 @@
 export * from "../../../domain/models/account";
 export * from "../../../domain/usecases/addAccount";
-export * from "../../../utils/email-validator-adapter";
+export * from "../../../main/adapters/validators/email-validator-adapter";
 export * from "../../protocols";
 export * from "../../protocols/email-validator";
 export * from "../../protocols/validation";
