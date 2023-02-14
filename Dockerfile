1c4c69baf113803f64fc7780e373802049f47128
Caio Sales <caio.cesar@liven.tech>
2023-02-14 14:15:57 -0300

    chore: docker scripts

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ WORKDIR /usr/src/clean-node-api
 
 COPY ./package.json .
 RUN npm install --only=prod
-COPY ./dist ./dist
+# COPY ./dist ./dist
 
-EXPOSE 5000
-CMD npm start
+# EXPOSE 5050
+# CMD npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
+  mongoUrl: process.env.MONGO_URL || "mongodb://mongo:27017/clean-node-api",
   port: process.env.PORT || 3333,
   secret: process.env.SECRET || "anySecret",
   salt: process.env.SALT ? Number(process.env.SALT) : 12,
