8c15bcff3c3deff579bdca7582d1e1d6ee85f899
Caio Sales <caio.cesar@liven.tech>
2023-03-11 18:39:27 -0300

    fix: imports at authentication factory

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -8,7 +8,6 @@ export class CompareFieldsValidation implements Validation {
   ) {}
 
   validate(input: any): Error | void {
-    console.log(input, input[this.fieldName], input[this.fieldToCompare]);
     if (input[this.fieldName] !== input[this.fieldToCompare]) {
       return new InvalidParamError(this.fieldToCompare);
     }
